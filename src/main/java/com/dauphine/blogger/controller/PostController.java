6943a01223a0df4d23ca82883fda4d89b3b1ccac
RicardoBOKA <ricardoboka.pro@gmail.com>
2024-04-23 19:23:01 +0200

    Endpoint Post
    Expose Post Edpoint

--- src/main/java/com/dauphine/blogger/controller/PostController.java
+++ src/main/java/com/dauphine/blogger/controller/PostController.java
@@ -1,10 +1,89 @@
 package com.dauphine.blogger.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.dauphine.blogger.models.Category;
+import com.dauphine.blogger.models.Post;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
-@RequestMapping("/v1/post")
+@RequestMapping("/v1/posts")
 public class PostController {
+    private final List<Post> temporaryPosts;
+
+    public PostController() {
+        temporaryPosts = new ArrayList<>();
+        Category sampleCategory = new Category(UUID.randomUUID(), "Sample Category");
+        temporaryPosts.add(new Post(UUID.randomUUID(), "Titre post n°1", "Contenu du post n°1", LocalDateTime.now(), sampleCategory));
+        temporaryPosts.add(new Post(UUID.randomUUID(), "Titre post n°2", "Contenu du post n°2", LocalDateTime.now(), sampleCategory));
+        temporaryPosts.add(new Post(UUID.randomUUID(), "Titre post n°3", "Contenu du post n°3", LocalDateTime.now(), sampleCategory));
+    }
+
+    /**
+     * Récupère tous les posts disponibles.
+     * @return une liste de tous les posts.
+     */
+    @GetMapping
+    public List<Post> getAllPosts() {
+        return temporaryPosts;
+    }
+
+    /**
+     * Récupère un posts par son ID.
+     * @return un post.
+     */
+    @GetMapping("/{id}")
+    public Post getPostById(@PathVariable UUID id) {
+        Optional<Post> post = temporaryPosts.stream().filter(c -> c.getUuid().equals(id)).findFirst();
+        return post.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build()).getBody();
+    }
+
+    /**
+     * Crée un nouveau post à partir des données fournies et l'ajoute à la liste des posts.
+     * @param post le post à ajouter, fourni dans le corps de la requête.
+     * @return une ResponseEntity contenant le post créé avec un statut HTTP 201.
+     */
+
+    @PostMapping
+    public ResponseEntity<Post> postPost(@RequestBody Post post) {
+        post.setUuid(UUID.randomUUID());
+        post.setCrated_date(LocalDateTime.now());
+        temporaryPosts.add(post);
+        return ResponseEntity.status(HttpStatus.CREATED).body(post);
+    }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Post> putPost(@PathVariable UUID id, @RequestBody Post updatedPost) {
+        Optional<Post> postOptional = temporaryPosts.stream().filter(p -> p.getUuid().equals(id)).findFirst();
+
+        if (postOptional.isPresent()) {
+            System.out.println("TEST");
+            Post post = postOptional.get();
+            post.setTitle(updatedPost.getTitle());
+            post.setContent(updatedPost.getContent());
+            return ResponseEntity.ok(post);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePost(@PathVariable UUID id) {
+        boolean isRemoved = temporaryPosts.removeIf(p -> p.getUuid().equals(id));
+        if (isRemoved) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
 }
--- src/main/java/com/dauphine/blogger/models/Post.java
+++ src/main/java/com/dauphine/blogger/models/Post.java
@@ -1,4 +1,58 @@
 package com.dauphine.blogger.models;
 
+import com.fasterxml.jackson.annotation.JsonTypeId;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.UUID;
+
 public class Post {
+    private UUID uuid;
+    private String title;
+    private String content;
+    private LocalDateTime crated_date;
+
+    private Category category_Id;
+
+    public Post() {}
+
+    public Post(UUID uuid, String title, String content, LocalDateTime crated_date, Category category_Id) {
+        this.uuid = uuid;
+        this.title = title;
+        this.content = content;
+        this.crated_date = crated_date;
+        this.category_Id = category_Id;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getCrated_date() {
+        return crated_date;
+    }
+
+    public void setCrated_date(LocalDateTime crated_date) {
+        this.crated_date = crated_date;
+    }
 }
